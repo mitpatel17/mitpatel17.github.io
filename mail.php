e64b033ee73eaaff3dc79dbf7e1880c2d2113c09
Mit Patel <mitpatel9828@gmail.com>
2022-05-07 00:43:07 -0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,20 +1,45 @@
 <?php
-    $to = "mitpatel9828@gmail.com"; 
-    $from = $_SERVER['PHP_SELF']." ".$_POST["email"];
-    $fname = $_POST["name"];
-    $email = $_POST["email"];
-    $lname = $_POST["subject"];
-    $headers = "From: $from";
-    $message = $_POST["message"];
+    
+    if (isset($_POST["email"] && $_POST["email"] != " ")){
+        $fname = $_POST["name"];
+        $email = $_POST["email"];
+        $lname = $_POST["subject"];
+        $message = $_POST["message"];
 
-    $body = "User Message \n"; 
-    $body .= " \n\n\t Name: ".$name;
-    $body .= " \n\n\t Email: ".$email;
-    $body .= " \n\n\t Subject: ".$subject;
-    $body .= " \n\n\t Message: ".$message;
+        $to = "mitpatel9828@gmail.com"; 
 
-    if(mail($to, $subject, $body, $headers)){
-        echo '<label class="success">Sent your <b>e-mail.</b></label>';
-    }else{
-        echo '<label class="error">Something went wrong! please try again.</label>';
+        $body = "User Message \n"; 
+        $body .= " \n\n\t Name: ".$name;
+        $body .= " \n\n\t Email: ".$email;
+        $body .= " \n\n\t Subject: ".$subject;
+        $body .= " \n\n\t Message: ".$message;
+        
+        if(mail($to, $subject, $body, $headers)){
+            echo '<label class="success">Sent your <b>e-mail.</b></label>';
+        }else{
+            echo '<label class="error">Something went wrong! please try again.</label>';
+        }
+
     }
+    
+    
+    
+    // $to = "mitpatel9828@gmail.com"; 
+    // $from = $_SERVER['PHP_SELF']." ".$_POST["email"];
+    // $fname = $_POST["name"];
+    // $email = $_POST["email"];
+    // $lname = $_POST["subject"];
+    // $headers = "From: $from";
+    // $message = $_POST["message"];
+
+    // $body = "User Message \n"; 
+    // $body .= " \n\n\t Name: ".$name;
+    // $body .= " \n\n\t Email: ".$email;
+    // $body .= " \n\n\t Subject: ".$subject;
+    // $body .= " \n\n\t Message: ".$message;
+
+    // if(mail($to, $subject, $body, $headers)){
+    //     echo '<label class="success">Sent your <b>e-mail.</b></label>';
+    // }else{
+    //     echo '<label class="error">Something went wrong! please try again.</label>';
+    // }
